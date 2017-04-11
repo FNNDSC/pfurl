88af40a42784e62bc5d765d07e45e87851ee43be
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2017-04-11 18:06:03 -0400

    Initial checkin.

---
+++ pfurl/__init__.py
@@ -0,0 +1,4 @@
+from ._colors import Colors
+from .debug   import debug
+from .message import Message
+from .purl    import Purl
---
+++ pfurl/tests/test_pfurl.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+from purl import purl
+
+class TestPfurl(TestCase):
+    def test_pfurl_constructor(self):
+        #myMan = purl()
+        # didn't crash
+        self.assertTrue(True)

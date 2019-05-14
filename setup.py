2d4279d6b92ea074a1c80216896733e7d0bfeb79
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2019-05-14 12:16:54 -0400

    Minor cosmetic changes and version bump (#28)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pfurl',
-      version          =   '1.3.20.2',
+      version          =   '1.3.20.4',
       description      =   '(Python) Path-File URL comms',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',

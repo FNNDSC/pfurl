c37e57b5dc03a81a15e566f2d325a7dd1047ac10
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2021-10-28 13:01:27 -0400

    Added CI workflow script & version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pfurl',
-      version          =   '2.3.0.1',
+      version          =   '2.3.0.2',
       description      =   '(Python) Path-File URL comms',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
